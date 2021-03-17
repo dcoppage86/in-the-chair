71eca5b19263605e5484eff1a1dfac4b5946915e
dcoppage86 <dakotakcoppage@me.com>
2021-03-17 00:02:26 -0600

    building create method and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
     end
 
     def show
@@ -21,6 +22,12 @@ class UsersController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(user).permit(:first_name, :last_name, :email, :password)
     end
     
 end
